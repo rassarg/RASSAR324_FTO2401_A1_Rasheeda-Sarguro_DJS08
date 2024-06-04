a6981e0155d3a1f4fd805699fae5567d587b30b6
rasheeda <raskieda@gmail.com>
2024-06-04 20:24:36 +0200

    added mirage JS server and created initial routes for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+import "../server.js";
+
 /**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
+ * Challenge: Create the Vans list page, Route, and Link
+ *
+ * 1. Create a Vans component in the "pages" directory. For now,
+ *    just render <h1>Vans page goes here 🚐</h1>
+ * 2. Create a Route for the Vans page on the /vans route
+ * 3. Add a Link in the nav bar to the Vans route
  */
 
-function App() {
-
+export default function App() {
   return (
-    <h1>Start here</h1>
-  )
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
