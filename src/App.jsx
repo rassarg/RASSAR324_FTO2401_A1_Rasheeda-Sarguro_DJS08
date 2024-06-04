4f8bbfbd04e8c75b91789f655d549d48c4c603da
rasheeda <raskieda@gmail.com>
2024-06-04 21:04:47 +0200

    created Vans useState and mapped over server.js file data to render van divs

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,6 @@ import Vans from "../pages/Vans";
 
 import "../server.js";
 
-/**
- * Challenge: Create the Vans list page, Route, and Link
- *
- * 1. Create a Vans component in the "pages" directory. For now,
- *    just render <h1>Vans page goes here 🚐</h1>
- * 2. Create a Route for the Vans page on the /vans route
- * 3. Add a Link in the nav bar to the Vans route
- */
-
 export default function App() {
   return (
     <BrowserRouter>
