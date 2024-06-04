e9674dd52e59153e6d1f41126a15b7f235c915d7
rasheeda <raskieda@gmail.com>
2024-06-04 20:31:46 +0200

    created Vans list page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Vans from "../pages/Vans";
 
 import "../server.js";
 
@@ -24,11 +25,13 @@ export default function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
