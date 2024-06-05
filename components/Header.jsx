efd0231a75775021cf97554ebe15a960b7e0e975
rasheeda <raskieda@gmail.com>
2024-06-05 15:52:39 +0200

    created Host components adn set up routes for the pages

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
         #VanLife
       </Link>
       <nav>
+        <Link to="/host">Host</Link>
         <Link to="/about">About</Link>
         <Link to="/vans">Vans</Link>
       </nav>
--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Vans from "../pages/Vans";
-import VanDetail from "../pages/VanDetail";
+import Vans from "../pages/Vans/Vans.jsx";
+import VanDetail from "../pages/Vans/VanDetail.jsx";
 import Layout from "../components/Layout";
+import Dashboard from "../pages/Host/Dashboard.jsx";
+import Income from "../pages/Host/Income.jsx";
+import Reviews from "../pages/Host/Reviews.jsx";
 
 import "../server.js";
 
@@ -18,6 +21,9 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
