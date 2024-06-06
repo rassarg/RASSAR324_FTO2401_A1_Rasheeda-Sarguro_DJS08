339cb5f3ba27ebb069a4c8db80e5ee15ad052c7e
rasheeda <raskieda@gmail.com>
2024-06-06 18:46:48 +0200

    reverted to pre-Firebase state

--- api.js
+++ api.js
@@ -1,38 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// A function whose only purpose is to delay execution
+// for the specified # of milliseconds when used w/ `await`
+// e.g. inside an async function:
+// await sleep(2000)  => pauses the function for 2 seconds before moving on
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(() => resolve(), ms));
+}
 
-// Web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCy5A_XGvu_n9LTNEx5MgV_MpMyvKNAj_w",
-  authDomain: "vanlife-f563c.firebaseapp.com",
-  projectId: "vanlife-f563c",
-  storageBucket: "vanlife-f563c.appspot.com",
-  messagingSenderId: "469634336897",
-  appId: "1:469634336897:web:046ba9de896a36b1c42580",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Refactoring the fetching functions below
-const vansCollectionRef = collection(db, "vans");
-
-// export async function getVans(id) {
-//   const url = id ? `/api/vans/${id}` : "/api/vans";
-//   const res = await fetch(url);
-//   if (!res.ok) {
-//     throw {
-//       message: "Failed to fetch vans",
-//       statusText: res.statusText,
-//       status: res.status,
-//     };
-//   }
-//   const data = await res.json();
-//   return data.vans;
-// }
+export async function getVans(id) {
+  const url = id ? `/api/vans/${id}` : "/api/vans";
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw {
+      message: "Failed to fetch vans",
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+  const data = await res.json();
+  return data.vans;
+}
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
